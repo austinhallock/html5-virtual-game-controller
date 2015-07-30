283cf158d22d0a86c9f180ad9d81850031ba927c
Viljami Peltola <viljami.peltola@iki.fi>
2015-07-30 14:45:26 +1000

    Minified file get .min suffix.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var serve = require('gulp-serve');
 var uglify = require('gulp-uglify');
 
 gulp.task('default', function(){
   gulp.src('src/gamecontroller.js')
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('dist/gamecontroller.min.js'));
+  .pipe(uglify({mangle: {
+    GameController: false,
+    window: false,
+    exports: false,
+    module: false,
+    requestAnimationFrame: false,
+    cancelAnimationFrame: false
+  }}))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('serve', serve(['src', 'tests']));
